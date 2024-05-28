51d99d84adae8641d2aed96bb52dd6f3fd24c9a5
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2024-05-29 00:11:59 +0700

    Revert "Tuuh"

--- dpt_service_management/models/__init__.py
+++ dpt_service_management/models/__init__.py
@@ -2,4 +2,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import service
-from . import uom_uom_inherit
--- dpt_service_management/models/uom_uom_inherit.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _
-from datetime import datetime
-
-
-class UomUom(models.Model):
-    _inherit = 'uom.uom'
-
-    description = fields.Html(string='Description')
