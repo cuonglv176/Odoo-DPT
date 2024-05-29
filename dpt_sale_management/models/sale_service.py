c5ccf460f2109d5e57846c4dba0bfe08e3a69525
binhdh <onghoan2009@gmail.com>
2024-05-29 07:20:41 +0700

    dieu chinh gia

--- dpt_sale_management/models/sale_service.py
+++ dpt_sale_management/models/sale_service.py
@@ -40,7 +40,16 @@ class SaleOrder(models.Model):
         pass
 
     def action_change_price(self):
-        pass
+        return {
+            'name': "Change Price",
+            'type': 'ir.actions.act_window',
+            'res_model': 'dpt.sale.change.price',
+            'target': 'new',
+            'views': [[False, 'form']],
+            'context': {
+                'default_order_id': self.id,
+            },
+        }
 
     @api.depends('sale_service_ids.amount_total')
     def _compute_service_amount(self):
@@ -57,7 +66,62 @@ class SaleOrder(models.Model):
 class DPTSaleChangePrice(models.Model):
     _name = 'dpt.sale.change.price'
 
+    def _default_data_service(self):
+        order_id = self.env['sale.order'].browse(self.env.context.get('active_id') or self.env.context.get('default_order_id'))
+        services = []
+        if order_id:
+            for r in order_id.sale_service_ids:
+                services.append((0, 0, {
+                    'service_id': r.service_id.id,
+                    'qty': r.qty,
+                    'price': r.price,
+                    'currency_id': order_id.company_id.currency_id.id,
+                    'amount_total': r.amount_total,
+                }))
+        return services or False
 
-class DPTSaleChangePriceLine(models.Model):
-    _name = 'dpt.sale.change.price.line'
+    def _default_data_product(self):
+        order_id = self.env['sale.order'].browse(self.env.context.get('active_id') or self.env.context.get('default_order_id'))
+        products = []
+        if order_id:
+            for r in order_id.order_line:
+                products.append((0, 0, {
+                    'product_id': r.product_template_id.id,
+                    'qty': r.product_uom_qty,
+                    'price': r.price_unit,
+                    'currency_id': order_id.company_id.currency_id.id,
+                    'amount_total': r.price_subtotal,
+                }))
+        return products or False
+
+    order_id = fields.Many2one('sale.order')
+    service_ids = fields.One2many('dpt.sale.change.price.servie.line', 'parent_id', default=_default_data_service)
+    product_ids = fields.One2many('dpt.sale.change.price.product.line', 'parent_id', default=_default_data_product)
+
+    def send_request(self):
+        return
+
+
+class DPTSaleChangePriceServiceLine(models.Model):
+    _name = 'dpt.sale.change.price.servie.line'
+
+    parent_id = fields.Many2one('dpt.sale.change.price')
+    service_id = fields.Many2one('dpt.service.management', string='Service')
+    qty = fields.Float(string='QTY')
+    price = fields.Monetary(currency_field='currency_id', string='Price')
+    change_price = fields.Monetary(currency_field='currency_id', string='Change Price')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    amount_total = fields.Float(string="Amount Total")
+
+
+class DPTSaleChangePriceProductLine(models.Model):
+    _name = 'dpt.sale.change.price.product.line'
+
+    parent_id = fields.Many2one('dpt.sale.change.price')
+    product_id = fields.Many2one('product.template', string='Product')
+    qty = fields.Float(string='QTY')
+    price = fields.Monetary(currency_field='currency_id', string='Price')
+    change_price = fields.Monetary(currency_field='currency_id', string='Change Price')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    amount_total = fields.Float(string="Amount Total")
 
