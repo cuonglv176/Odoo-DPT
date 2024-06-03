53110091b4ba90ae5ebd76dc5d911bc729eca6fd
cuonglv176 <brianlee.lc@gmail.com>
2024-06-03 16:31:38 +0700

    Update export import

--- dpt_export_import/__manifest__.py
+++ dpt_export_import/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'DPT Sale Department',
+    'name': 'DPT Export Import',
     'version': '1.0',
     'summary': 'Manage Sale Information for each department',
     'category': 'Services',
     'author': 'CuongLV',
     'maintainer': 'Your Name',
     'website': 'http://dpt.com',
-    'depends': ['sale', 'sale_management', 'dpt_service_management', 'hr', 'purchase', 'stock', 'dpt_sale_management',
-                'dpt_export_import'],
+    'depends': ['sale', 'sale_management', 'dpt_service_management', 'hr', 'purchase', 'stock'],
     'data': [
-        'views/sale_order.xml',
+        'views/export_import_view.xml',
     ],
 }
