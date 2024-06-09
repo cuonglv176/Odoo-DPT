bcce33414a73f2af3bdeb59310d70b5ba00d2475
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2024-06-09 13:50:54 +0700

    fix app

--- dpt_sale_approvals/models/approval_request.py
+++ dpt_sale_approvals/models/approval_request.py
@@ -7,6 +7,16 @@ class ApprovalRequest(models.Model):
     _inherit = 'approval.request'
 
     sale_id = fields.Many2one('sale.order', string='Sale Order')
-    sale_service_ids = fields.One2many('dpt.sale.service.management','approval_id', string='Sale Service')
-    order_line_ids = fields.One2many('sale.order.line','approval_id', string='Sale Order Line')
+    sale_service_ids = fields.One2many('dpt.sale.service.management', 'approval_id', string='Sale Service')
+    order_line_ids = fields.One2many('sale.order.line', 'approval_id', string='Sale Order Line')
     sequence_code = fields.Char(string="Code", related='category_id.sequence_code')
+
+    def action_approve(self, approver=None):
+        res = super(ApprovalRequest, self).action_approve(approver)
+        approver = self.approver_ids.filtered(lambda sp: sp.status == 'approved')
+        if not approver:
+            for sale_service_id in self.sale_service_ids:
+                sale_service_id.price = sale_service_id.new_price
+            for order_line_id in self.order_line_ids:
+                order_line_id.price_unit = sale_service_id.new_price_unit
+        return res
