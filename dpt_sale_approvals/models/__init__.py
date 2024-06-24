572e2299b128f6a860c158fcc66760f280734901
BnK Solution <onghoan2009@gmail.com>
2024-06-24 14:09:33 +0700

    update rule approval pricelist

--- dpt_sale_approvals/models/__init__.py
+++ dpt_sale_approvals/models/__init__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import sale_order
-from . import sale_order_line
+# from . import sale_order_line
 from . import dpt_sale_service_management
 from . import approval_request
 from . import dpt_approval_sale_price
