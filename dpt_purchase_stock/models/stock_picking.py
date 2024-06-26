8caacde9cdcb7637896576aed6f320cc9846fc1e
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2024-06-26 10:41:04 +0700

    update logic po -> picking

--- dpt_purchase_stock/models/stock_picking.py
+++ dpt_purchase_stock/models/stock_picking.py
@@ -5,3 +5,18 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     package_ids = fields.One2many('purchase.order.line.package', 'picking_id', 'Packages')
+    move_ids_product = fields.One2many('stock.move', 'picking_id', string="Stock move",
+                                       domain=[('is_package', '=', False)])
+    sale_purchase_id = fields.Many2one(related="purchase_id.sale_id")
+
+    @api.onchange('package_ids')
+    def onchange_package(self):
+        # remove package move
+        package_move_ids = self.move_ids_without_package - self.move_ids_product
+        package_move_ids.unlink()
+        if self.purchase_id:
+            self.package_ids.update({
+                'purchase_id': self.purchase_id.id
+            })
+        # create package move
+        self.package_ids._create_stock_moves(self)
