53db63e9b1da89106b21a48b6dfb5898a9d343a6
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2024-06-29 00:00:50 +0700

    update order line

--- dpt_export_import/models/sale_order.py
+++ dpt_export_import/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, _
+from odoo import fields, models, api, _
 
 
 class SaleOrder(models.Model):
@@ -49,6 +49,56 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     dpt_export_import_line_ids = fields.One2many('dpt.export.import.line', 'sale_line_id', string='Declaration line')
+    product_code = fields.Char(string='HS Code', related='product_id.code')
+    declared_unit_price = fields.Monetary(string='Declared unit price', currency_field='currency_id')
+    declared_unit_total = fields.Monetary(string='Declared unit Total', currency_field='currency_id')
+    payment_exchange_rate = fields.Monetary(string='Payment exchange rate', currency_field='currency_id')
+    import_tax_rate = fields.Float(string='Import tax rate(%)')
+    vat_tax_rate = fields.Float(string='VAT rate(%)')
+    other_tax_rate = fields.Float(string='Other tax rate(%)')
+    import_tax_amount = fields.Monetary(string='Import tax Amount', currency_field='currency_id')
+    vat_tax_amount = fields.Monetary(string='VAT tax Amount', currency_field='currency_id')
+    other_tax_amount = fields.Monetary(string='Other tax Amount', currency_field='currency_id')
+    total_tax_amount = fields.Monetary(string='Total tax Amount', currency_field='currency_id')
+    state_export_import_line = fields.Selection([
+        ('draft', 'Nháp'),
+        ('eligible', 'Đủ điều kiện khai báo'),
+        ('declared', 'Tờ khai thông quan'),
+        ('released', 'Giải phóng'),
+        ('consulted', 'Tham vấn'),
+        ('post_control', 'Kiểm tra sau thông quan'),
+        ('cancelled', 'Huỷ')
+    ], string='State', default='draft', compute='compute_state_export_import_line')
+
+    @api.depends('dpt_export_import_line_ids', 'dpt_export_import_line_ids.state')
+    def compute_state_export_import_line(self):
+        for rec in self:
+            state_export_import_line = 'draft'
+            for dpt_export_import_line_id in rec.dpt_export_import_line_ids:
+                state_export_import_line = dpt_export_import_line_id.state
+            rec.state_export_import_line = state_export_import_line
+
+    @api.onchange('declared_unit_price', 'product_uom_qty')
+    def onchange_compute_declared_unit_total(self):
+        self.declared_unit_total = self.declared_unit_price * self.product_uom_qty
+
+    @api.onchange('product_id')
+    def onchange_compute_tax_rate(self):
+        self.import_tax_rate = self.product_id.dpt_tax_import
+        self.vat_tax_rate = self.product_id.dpt_tax
+
+    @api.onchange('import_tax_amount', 'declared_unit_total')
+    def onchange_compute_import_tax_amount(self):
+        self.import_tax_amount = self.declared_unit_total * self.import_tax_amount
+
+    @api.onchange('other_tax_rate', 'declared_unit_total')
+    def onchange_compute_other_tax_amount(self):
+        self.other_tax_amount = self.declared_unit_total * self.other_tax_rate
+
+    @api.onchange('other_tax_amount', 'import_tax_amount', 'declared_unit_total', 'vat_tax_rate')
+    def onchange_compute_vat_tax_amount(self):
+        self.vat_tax_amount = (
+                                      self.declared_unit_total + self.other_tax_amount + self.import_tax_amount) * self.vat_tax_rate
 
     def action_open_dpt_export_import_line(self):
         view_id = self.env.ref('dpt_export_import.view_dpt_export_import_line_form').id
