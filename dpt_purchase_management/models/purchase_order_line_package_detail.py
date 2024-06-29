b0189188da61fbad2ccde0d3a8ad126b46e09a70
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2024-06-29 09:19:27 +0700

    update code

--- dpt_purchase_management/models/purchase_order_line_package_detail.py
+++ dpt_purchase_management/models/purchase_order_line_package_detail.py
@@ -24,29 +24,14 @@ class PurchaseOrderLinePackageDetail(models.Model):
 
     @api.onchange('product_id')
     def onchange_product(self):
-        for item in self:
-            product_ids = self.env['product.product']
-            if item.package_id.purchase_id:
-                product_ids |= item.package_id.purchase_id.order_line.mapped('product_id')
-            if item.package_id.picking_id:
-                product_ids |= item.package_id.picking_id.move_ids_product.mapped('product_id')
-            if product_ids:
-                return {
-                    'domain': {
-                        'product_id': [('id', 'in', product_ids.ids)]
-                    }
-                }
-            else:
-                return {
-                    'domain': {
-                        'product_id': []
-                    }
-                }
-
-    @api.onchange('product_id')
-    def onchange_product(self):
         if self.product_id:
             product_in_po_line_id = self.package_id.purchase_id.order_line.filtered(
                 lambda pol: pol.product_id == self.product_id)
             if product_in_po_line_id:
                 self.uom_id = product_in_po_line_id.uom_id
+
+    @api.onchange('sale_line_id')
+    def onchange_sale_line(self):
+        if self.sale_line_id:
+            self.uom_id = self.sale_line_id.uom_id
+            self.product_id = self.sale_line_id.product_id
