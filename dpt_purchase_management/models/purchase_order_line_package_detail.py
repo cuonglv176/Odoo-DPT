843a0ab64ff9abe2325dc89b0a57be5cc633d683
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2024-06-29 09:12:52 +0700

    update code

--- dpt_purchase_management/models/purchase_order_line_package_detail.py
+++ dpt_purchase_management/models/purchase_order_line_package_detail.py
@@ -11,11 +11,42 @@ class PurchaseOrderLinePackageDetail(models.Model):
     uom_id = fields.Many2one('uom.uom', 'Unit')
     quantity = fields.Float('Quantity')
     package_id = fields.Many2one('purchase.order.line.package', 'Package')
+    product_ids = fields.Many2many('product.product', compute="_compute_product")
+
+    def _compute_product(self):
+        for item in self:
+            product_ids = self.env['product.product']
+            if item.package_id.purchase_id:
+                product_ids |= item.package_id.purchase_id.order_line.mapped('product_id')
+            if item.package_id.picking_id:
+                product_ids |= item.package_id.picking_id.move_ids_product.mapped('product_id')
+            item.product_ids = product_ids if product_ids else self.env['product.product'].search([])
+
+    @api.onchange('product_id')
+    def onchange_product(self):
+        for item in self:
+            product_ids = self.env['product.product']
+            if item.package_id.purchase_id:
+                product_ids |= item.package_id.purchase_id.order_line.mapped('product_id')
+            if item.package_id.picking_id:
+                product_ids |= item.package_id.picking_id.move_ids_product.mapped('product_id')
+            if product_ids:
+                return {
+                    'domain': {
+                        'product_id': [('id', 'in', product_ids.ids)]
+                    }
+                }
+            else:
+                return {
+                    'domain': {
+                        'product_id': []
+                    }
+                }
 
     @api.onchange('product_id')
     def onchange_product(self):
         if self.product_id:
             product_in_po_line_id = self.package_id.purchase_id.order_line.filtered(
-                lambda pol: pol.product_template_id == self.product_id.product_tmpl_id)
+                lambda pol: pol.product_id == self.product_id)
             if product_in_po_line_id:
                 self.uom_id = product_in_po_line_id.uom_id
