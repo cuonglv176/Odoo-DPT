b964c96e41d2018039a61c31d95a1c4911bd9c55
cuonglv176 <brianlee.lc@gmail.com>
2024-07-01 13:03:03 +0700

    Update service_inherit.py

--- dpt_helpdesk_ticket/models/service_inherit.py
+++ dpt_helpdesk_ticket/models/service_inherit.py
@@ -5,3 +5,11 @@ class DPTService(models.Model):
     _inherit = 'dpt.service.management'
 
     helpdesk_team_id = fields.Many2one('helpdesk.team', string='Helpdesk Team')
+
+    @api.model
+    def create(self, vals):
+        self.env['helpdesk.team'].create({
+            'name': vals.get('name'),
+            'company_id': 1,
+        })
+        return super(DPTService, self).create(vals)
