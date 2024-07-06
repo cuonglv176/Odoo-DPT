3580ff84ed1c5a5e8ec45f19f250d9ebaeb22bd7
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2024-07-07 01:30:04 +0700

    update approval

--- dpt_hr_custom/models/hr_department_inherit.py
+++ dpt_hr_custom/models/hr_department_inherit.py
@@ -7,4 +7,15 @@ class HrDepartment(models.Model):
 
     code = fields.Char('Code')
 
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for department in self:
+            if self.env.context.get('only_department_name', False):
+                department.complete_name = department.name
+            else:
+                if department.parent_id:
+                    department.complete_name = '%s / %s' % (department.parent_id.complete_name, department.name)
+                else:
+                    department.complete_name = department.name
+
 
