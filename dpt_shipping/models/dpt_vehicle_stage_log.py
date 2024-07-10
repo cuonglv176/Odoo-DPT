bf4fc3501422162a5c54d99f1bc92afc05bdb6cf
Ưng Hoàng Tú <tuuh@jeisys.com.vn>
2024-07-11 00:08:01 +0700

    update tracking

--- dpt_shipping/models/dpt_vehicle_stage_log.py
+++ dpt_shipping/models/dpt_vehicle_stage_log.py
@@ -5,11 +5,12 @@ from odoo import models, fields, api
 
 class DPTVehicleStageLog(models.Model):
     _name = 'dpt.vehicle.stage.log'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle')
+    vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle', tracking=True)
     shipping_slip_id = fields.Many2one('dpt.shipping.slip', 'Shipping Slip')
-    current_stage_id = fields.Many2one('dpt.vehicle.stage', 'Current Stage')
-    next_stage_id = fields.Many2one('dpt.vehicle.stage', 'Next Stage')
-    log_datetime = fields.Datetime('Log Datetime', default=lambda self: fields.Datetime.now())
-    log_time = fields.Float('Log Time')
-    description = fields.Text('Description')
+    current_stage_id = fields.Many2one('dpt.vehicle.stage', 'Current Stage', tracking=True)
+    next_stage_id = fields.Many2one('dpt.vehicle.stage', 'Next Stage', tracking=True)
+    log_datetime = fields.Datetime('Log Datetime', default=lambda self: fields.Datetime.now(), tracking=True)
+    log_time = fields.Float('Log Time', tracking=True)
+    description = fields.Text('Description', tracking=True)
