641d4e4f75f4fad387f6f270bd12c7c39b0d324a
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2024-07-12 06:07:38 +0700

    purchase smart button

--- dpt_sale_deposit/__manifest__.py
+++ dpt_sale_deposit/__manifest__.py
@@ -8,11 +8,12 @@
     'maintainer': 'Your Name',
     'website': 'http://dpt.com',
     'depends': ['sale', 'sale_management', 'dpt_service_management', 'uom', 'dpt_service_pricelist',
-                'dpt_sale_management', 'account'],
+                'dpt_sale_management', 'account', 'purchase'],
     'data': [
         'security/ir.model.access.csv',
         'views/sale_order.xml',
         'views/account_payment.xml',
+        'views/purchase_order.xml',
     ],
     'installable': True,
     'application': True,
--- dpt_sale_deposit/models/__init__.py
+++ dpt_sale_deposit/models/__init__.py
@@ -3,3 +3,4 @@
 
 from . import sale_order
 from . import account_payment
+from . import purchase_order
