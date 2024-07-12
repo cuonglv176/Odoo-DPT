9fc01b6fc6c91143527890735a6232ea0bb84c11
trunglen123 <len.trung.gttn@gmail.com>
2024-07-13 02:15:55 +0700

    [ADD] helpdesk ticket

--- dpt_helpdesk_ticket/models/helpdesk_ticket.py
+++ dpt_helpdesk_ticket/models/helpdesk_ticket.py
@@ -17,6 +17,169 @@ class HelpdeskTicket(models.Model):
     service_ids = fields.Many2many('dpt.service.management', compute='_compute_service_ids', store=True)
     pack_name = fields.Char(string='Mã pack', compute='_compute_pack_name', store=True)
     sale_id = fields.Many2one('sale.order', string='Đơn bán hàng')
+    user_sale_id = fields.Many2one('res.users', string='Nhân viên kinh doanh', related='sale_id.user_id', store=True)
+
+    def action_view_sale_order(self):
+        if not self.sale_id:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Cảnh báo',
+                    'type': 'warning',
+                    'message': 'Không tìm thấy đơn hàng',
+                    'sticky': True,
+                }
+            }
+        sale_id = self.sale_id
+        view_id = self.env.ref('sale.view_order_form').id
+        action = self.env['ir.actions.actions']._for_xml_id('sale.action_orders')
+        action.update({
+            'view_mode': 'form',
+            'views': [[view_id, 'form']],
+            'target': 'main',
+            'res_id': sale_id.id,
+        })
+        return action
+
+    def action_view_purchase_order(self):
+        if not self.purchase_id:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Cảnh báo',
+                    'type': 'warning',
+                    'message': 'Không tìm thấy đơn mua hàng',
+                    'sticky': True,
+                }
+            }
+        purchase_id = self.purchase_id
+        result = self.env['ir.actions.act_window']._for_xml_id('purchase.purchase_form_action')
+        view_id = self.env.ref('purchase.purchase_order_form').id
+        result.update({
+            'view_mode': 'form',
+            'views': [[view_id, 'form']],
+            'target': 'main',
+            'res_id': purchase_id.id,
+        })
+        return result
+
+    def action_view_stock_picking_order(self):
+        view_id = self.env.ref('stock.vpicktree').id
+        view_form_id = self.env.ref('stock.view_picking_form').id
+        stock_picking_ids = self.env['stock.picking'].search([
+            '|',
+            '|',
+            '|',
+            '|',
+            ('sale_id', '=', self.sale_id.id),
+            ('sale_purchase_id', '=', self.sale_id.id),
+            ('purchase_id', '=', self.purchase_id.id),
+            ('origin', '=', self.sale_id.name),
+            ('origin', '=', self.purchase_id.name),
+        ])
+        if not stock_picking_ids:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Cảnh báo',
+                    'type': 'warning',
+                    'message': 'Không tìm thấy phiếu kho',
+                    'sticky': True,
+                }
+            }
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'stock.picking',
+            'name': 'Phiếu kho',
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', stock_picking_ids.ids)],
+            'views': [[view_id, 'tree'], [view_form_id, 'form']],
+        }
+
+    def action_view_approval_request(self):
+        if not self.sale_id:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Cảnh báo',
+                    'type': 'warning',
+                    'message': 'Không tìm thấy đơn hàng',
+                    'sticky': True,
+                }
+            }
+        sale_id = self.sale_id
+        view_id = self.env.ref('approvals.approval_request_view_kanban').id
+        view_tree_id = self.env.ref('approvals.approval_request_view_tree').id
+        view_form_id = self.env.ref('approvals.approval_request_view_form').id
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'approval.request',
+            'name': 'Phê duyệt',
+            'view_mode': 'kanban,tree,form',
+            'domain': [('sale_id', '=', sale_id.id)],
+            'views': [[view_id, 'kanban'], [view_tree_id, 'tree'], [view_form_id, 'form']],
+        }
+
+
+    def action_view_contract(self):
+        if not self.purchase_id:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Cảnh báo',
+                    'type': 'warning',
+                    'message': 'Không tìm thấy đơn mua hàng',
+                    'sticky': True,
+                }
+            }
+        purchase_id = self.purchase_id
+        if not purchase_id.contract_id:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Cảnh báo',
+                    'type': 'warning',
+                    'message': 'Không tìm thấy hợp đồng',
+                    'sticky': True,
+                }
+            }
+        contract_id = purchase_id.contract_id
+        return {
+            'name': "Hợp đồng",
+            'type': 'ir.actions.act_window',
+            'res_model': 'dpt.contract.management',
+            'res_id': contract_id.id,
+            'target': 'self',
+            'view_mode': 'form',
+            'view_id': self.env.ref('dpt_contract_management.view_service_form').id,
+        }
+
+
+    @api.onchange('sale_id')
+    def _onchange_sale_id(self):
+        if not self.sale_id:
+            return False
+        sale_id = self.sale_id
+        self.service_lines_ids = [
+            (0, 0, {
+                'sequence': line.sequence,
+                'currency_id': line.currency_id.id,
+                'service_id': line.service_id.id,
+                'description': line.description,
+                'qty': line.qty,
+                'uom_id': line.uom_id.id,
+                'price': line.price,
+                'amount_total': line.amount_total,
+                'department_id': line.department_id.id,
+            })
+        for line in sale_id.sale_service_ids]
+
 
     @api.depends(
         'purchase_id',
@@ -79,6 +242,7 @@ class DPTSaleChangePriceServiceLine(models.Model):
     currency_id = fields.Many2one('res.currency', string='Currency')
     amount_total = fields.Float(string="Amount Total")
     status = fields.Char(string='Status')
+    department_id = fields.Many2one('hr.department', related='parent_id.department_id')
 
 
 
