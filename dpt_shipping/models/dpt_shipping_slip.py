3fe2f9d8dbfdb80973a0267c79faf6a3487063c2
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2024-07-14 01:20:09 +0700

    Update shipping slip

--- dpt_shipping/models/dpt_shipping_slip.py
+++ dpt_shipping/models/dpt_shipping_slip.py
@@ -10,21 +10,38 @@ class DPTShippingSlip(models.Model):
     name = fields.Char('Name')
     transfer_code = fields.Char('Transfer Code')
     transfer_code_chinese = fields.Char('Transfer Code in Chinese')
-    picking_ids = fields.Many2many('stock.picking', 'stock_picking_in_shipping_rel', 'shipping_slip_id', 'picking_id',
-                                   string='Picking')
-    transfer_picking_ids = fields.Many2many('stock.picking', 'stock_picking_transfer_shipping_rel', 'shipping_slip_id',
-                                            'picking_id', string='Transfer Picking')
-    sale_ids = fields.Many2many('sale.order', string='Sale Order')
+    out_picking_ids = fields.Many2many('stock.picking', 'stock_picking_out_shipping_rel', 'shipping_slip_id',
+                                       'picking_id', string='Out Picking', domain="[('valid_cutlist','=', True)]")
+    export_import_ids = fields.Many2many('dpt.export.import', string='Export Import', compute="_compute_information")
+    in_picking_ids = fields.Many2many('stock.picking', 'stock_picking_in_shipping_rel', 'shipping_slip_id',
+                                      'picking_id', string='In Picking', domain="[('valid_cutlist','=', True)]")
+    sale_ids = fields.Many2many('sale.order', string='Sale Order', compute="_compute_information")
     vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle')
     vehicle_country = fields.Selection(related='vehicle_id.country')
     vn_vehicle_stage_id = fields.Many2one('dpt.vehicle.stage', 'Vehicle Stage', domain=[('country', '=', 'vietnamese')])
     cn_vehicle_stage_id = fields.Many2one('dpt.vehicle.stage', 'Vehicle Stage', domain=[('country', '=', 'chinese')])
     vehicle_stage_log_ids = fields.One2many('dpt.vehicle.stage.log', 'shipping_slip_id', 'Vehicle Stage Log')
     send_shipping_id = fields.Many2one('dpt.shipping.slip', 'Shipping Sent')
-    receive_shipping_ids = fields.One2many('dpt.shipping.slip', 'send_shipping_id', 'Shipping Receive')
     vehicle_driver_id = fields.Many2one(related="vehicle_id.driver_id")
     vehicle_license_plate = fields.Char(related="vehicle_id.license_plate")
     vehicle_driver_phone = fields.Char(compute="_compute_vehicle_driver_phone")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('ready_get_car', 'Ready Get Car'),
+        ('start_get_car', 'Start Get Car'),
+        ('finish_get_car', 'Finish Get Car'),
+        ('start_drop_good', 'Start Drop Good'),
+        ('finish_drop_good', 'Finish Drop Good'),
+        ('declared', 'Declared'),
+        ('move_other_car', 'Move Other Car'),
+        ('finish', 'Finish'),
+    ], default='draft', string='State')
+
+    def _compute_information(self):
+        for item in self:
+            item.sale_ids = (item.in_picking_ids | item.out_picking_ids).mapped('sale_purchase_id')
+            item.export_import_ids = (item.in_picking_ids | item.out_picking_ids).mapped('sale_purchase_id').mapped(
+                'dpt_export_import_ids')
 
     def _compute_vehicle_driver_phone(self):
         for item in self:
@@ -84,12 +101,6 @@ class DPTShippingSlip(models.Model):
                 'transfer_code_chinese': item.transfer_code_chinese,
             })
 
-    @api.onchange('sale_ids')
-    def onchange_sale_get_picking(self):
-        picking_ids = self.env['stock.picking'].sudo().search(
-            [('sale_id', 'in', self.sale_ids._origin.ids), ('picking_type_code', '=', 'incoming')])
-        self.picking_ids = picking_ids.filtered(lambda sp: sp.is_main_incoming).ids
-
     def action_create_shipping_slip_receive(self):
         action = self.env.ref('dpt_shipping.dpt_shipping_split_wizard_action').sudo().read()[0]
         action['context'] = {
@@ -98,3 +109,27 @@ class DPTShippingSlip(models.Model):
             'default_available_sale_ids': self.sale_ids.ids,
         }
         return action
+
+    def action_ready_get_car(self):
+        self.state = 'ready_get_car'
+
+    def action_start_get_car(self):
+        self.state = 'start_get_car'
+
+    def action_finish_get_car(self):
+        self.state = 'finish_get_car'
+
+    def action_start_drop_good(self):
+        self.state = 'start_drop_good'
+
+    def action_finish_drop_good(self):
+        self.state = 'finish_drop_good'
+
+    def action_declared(self):
+        self.state = 'declared'
+
+    def action_move_other_car(self):
+        self.state = 'move_other_car'
+
+    def action_finish(self):
+        self.state = 'finish'
