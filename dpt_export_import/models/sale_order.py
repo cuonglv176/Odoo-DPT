86f2d5c5fa290d56576a05ace73b55cb85fe6b46
cuonglv176 <brianlee.lc@gmail.com>
2024-08-08 16:38:34 +0700

    Update sale_order.py

--- dpt_export_import/models/sale_order.py
+++ dpt_export_import/models/sale_order.py
@@ -91,14 +91,6 @@ class SaleOrderLine(models.Model):
         ('cancelled', 'Huỷ')
     ], string='State', default='draft', compute='compute_state_export_import_line')
 
-    @api.model
-    def create(self, vals_list):
-        res = super(SaleOrderLine, self).create(vals_list)
-        for dpt_export_import_line_id in res.dpt_export_import_line_ids:
-            dpt_export_import_line_id.dpt_uom1_id = res.product_uom
-            dpt_export_import_line_id.dpt_sl1 = res.product_uom_qty
-        return res
-
     def write(self, vals):
         res = super(SaleOrderLine, self).write(vals)
         if 'product_uom' in vals or 'product_uom_qty' in vals:
