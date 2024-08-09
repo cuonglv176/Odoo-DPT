4215592bb63393464ab6776cd8674502b376d16c
cuonglv176 <brianlee.lc@gmail.com>
2024-08-09 09:24:27 +0700

    Update res_partner.py

--- dpt_contract_management/models/res_partner.py
+++ dpt_contract_management/models/res_partner.py
@@ -11,15 +11,15 @@ class ResPartner(models.Model):
         for record in self:
             record.count_contract = self.env['dpt.contract.management'].search_count([('partner_id', '=', self.id)])
 
-    @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
-        args = args or []
-        current_user = self.env.user
-        if not self.env.user.has_group("sales_team.group_sale_salesman_all_leads"):
-            if self.env.user.has_group('sales_team.group_sale_salesman'):
-                args += [('user_id', '=', current_user.id)]
-        partner = self.sudo().search(args, limit=limit)
-        return partner.name_get()
+    # @api.model
+    # def name_search(self, name='', args=None, operator='ilike', limit=100):
+    #     args = args or []
+    #     current_user = self.env.user
+    #     if not self.env.user.has_group("sales_team.group_sale_salesman_all_leads"):
+    #         if self.env.user.has_group('sales_team.group_sale_salesman'):
+    #             args += [('user_id', '=', current_user.id)]
+    #     partner = self.sudo().search(args, limit=limit)
+    #     return partner.name_get()
 
     def create_new_contract(self):
         return {
