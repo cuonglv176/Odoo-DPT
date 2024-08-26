00c136150b9eb2d311a1e44f2e55358c050df46d
cuonglv176 <brianlee.lc@gmail.com>
2024-08-26 10:32:49 +0700

    update

--- dpt_hr_custom/models/hr_employee_inherit.py
+++ dpt_hr_custom/models/hr_employee_inherit.py
@@ -13,8 +13,16 @@ class HrEmployee(models.Model):
         ('l4', 'L4'),
         ('l5', 'L5')
     ], default='l1', string='Cấp bậc')
+    parent_department_id = fields.Many2one('hr.department', string='Phòng')
+    center_code = fields.Char(string='Trung tâm')
+    bod_code = fields.Char(string='Mã BOD')
 
-
+    @api.onchange('job_id')
+    def onchange_job_id(self):
+        self.department_id = self.job_id.department_id
+        self.parent_department_id = self.job_id.parent_department_id
+        self.center_code = self.job_id.center_code
+        self.bod_code = self.job_id.bod_code
 
 class HrEmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
@@ -27,3 +35,6 @@ class HrEmployeePublic(models.Model):
         ('l4', 'L4'),
         ('l5', 'L5')
     ], default='l1', string='Cấp bậc', related='employee_id.dpt_level', compute_sudo=True)
+    parent_department_id = fields.Many2one('hr.department', string='Phòng', related='employee_id.parent_department_id', compute_sudo=True)
+    center_code = fields.Char(string='Trung tâm', related='employee_id.center_code', compute_sudo=True)
+    bod_code = fields.Char(string='Mã BOD', related='employee_id.bod_code', compute_sudo=True)
