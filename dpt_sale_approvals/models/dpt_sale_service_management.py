00a4cbf7266410957494f8cafa7c54eda2ea3500
BnK Solution <onghoan2009@gmail.com>
2024-09-06 10:35:20 +0700

    update tinh gia

--- dpt_sale_approvals/models/dpt_sale_service_management.py
+++ dpt_sale_approvals/models/dpt_sale_service_management.py
@@ -74,6 +74,7 @@ class DPTSaleServiceManagement(models.Model):
             if rec.price_status not in ('not_calculate', 'calculated') or approved:
                 continue
             rec.price_status = 'not_calculate'
+            not_approved = rec.approval_id.filtered(lambda approval: approval.request_status in ('pending', 'new'))
             if rec.sale_id.state == 'draft':
                 if not_approved:
                     rec.price_status = 'wait_approve'
@@ -83,7 +84,6 @@ class DPTSaleServiceManagement(models.Model):
                 else:
                     rec.price_status = 'calculated'
             elif rec.sale_id.state == 'wait_price':
-                not_approved = rec.approval_id.filtered(lambda approval: approval.request_status in ('pending', 'new'))
                 if not_approved:
                     rec.price_status = 'wait_approve'
                     continue
