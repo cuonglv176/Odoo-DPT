3a9ebf7dd9c1aaaf0407c0f1f675a3bf46b195ff
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2024-09-14 21:15:56 +0700

    Update main.py

--- dpt_zalo_oa/controllers/main.py
+++ dpt_zalo_oa/controllers/main.py
@@ -1,17 +1,22 @@
 from odoo import http
+from odoo.http import request
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
 class ZaloController(http.Controller):
-    @http.route('/zalo/callback', type='http', auth='public')
+    @http.route('/zalo/callback', type='http', auth='public', csrf=False)
     def zalo_callback(self, **kwargs):
         code = kwargs.get('code')
         if code:
             # Lấy access token sử dụng mã code
-            access_token = self.env['base.automation'].get_access_token(code)
-            _logger.info("TOKENT>>>>>>>>>>>>>>>>>>>")
-            _logger.info(access_token)
-            return http.Response(f"Access Token: {access_token}")
-        return http.Response("No code found")
+            try:
+                access_token = request.env['base.automation'].sudo().get_access_token(code)
+                _logger.info("Access Token Retrieved: %s", access_token)
+                return request.make_response(f"Access Token: {access_token}")
+            except Exception as e:
+                _logger.error("Error retrieving access token: %s", str(e))
+                return request.make_response(f"Error: {str(e)}", status=500)
+        _logger.warning("No code found in the callback")
+        return request.make_response("No code found", status=400)
