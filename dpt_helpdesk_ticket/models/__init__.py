4733a1e876975017f6f2c31aabae9fd1217ae8e9
Ưng Hoàng Tú <tuuh@jeisys.com.vn>
2024-10-15 13:43:49 +0700

    update edit from sale order to ticket

--- dpt_helpdesk_ticket/models/__init__.py
+++ dpt_helpdesk_ticket/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import dpt_sale_service_management
 from . import helpdesk_stage
 from . import helpdesk_ticket
 from . import sale_order
