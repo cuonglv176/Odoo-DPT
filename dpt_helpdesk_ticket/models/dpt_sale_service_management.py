f247ac304d602f0bf7f4199e7b39ae5f5c5b5e38
cuonglv176 <brianlee.lc@gmail.com>
2024-10-16 09:42:29 +0700

    Revert "update code"
    This reverts commit 7c4c7f9ff728567552863f6a242a050fb1befc15.

--- dpt_helpdesk_ticket/models/dpt_sale_service_management.py
+++ dpt_helpdesk_ticket/models/dpt_sale_service_management.py
@@ -4,30 +4,30 @@ from odoo import models, fields, api, _
 class DPTSaleServiceManagement(models.Model):
     _inherit = 'dpt.sale.service.management'
 
-    # def write(self, vals):
-    #     if 'service_id' in vals:
-    #         return super().write(vals)
-    #     for item in self:
-    #         ticket_line_ids = item.sale_id.ticket_ids.mapped('service_lines_ids').filtered(
-    #             lambda sl: sl.service_id.id == item.service_id.id and sl.uom_id.id == item.uom_id.id)
-    #         if not ticket_line_ids:
-    #             continue
-    #         service_line_ticket_vals = {}
-    #         if 'uom_id' in vals:
-    #             service_line_ticket_vals.update({
-    #                 'uom_id': vals.get('uom_id')
-    #             })
-    #         if 'price' in vals:
-    #             service_line_ticket_vals.update({
-    #                 'price': vals.get('price')
-    #             })
-    #         if 'qty' in vals:
-    #             service_line_ticket_vals.update({
-    #                 'qty': vals.get('qty')
-    #             })
-    #         if 'amount_total' in vals:
-    #             service_line_ticket_vals.update({
-    #                 'amount_total': vals.get('amount_total')
-    #             })
-    #         ticket_line_ids.write(service_line_ticket_vals)
-    #     return super().write(vals)
+    def write(self, vals):
+        if 'service_id' in vals:
+            return super().write(vals)
+        for item in self:
+            ticket_line_ids = item.sale_id.ticket_ids.mapped('service_lines_ids').filtered(
+                lambda sl: sl.service_id.id == item.service_id.id and sl.uom_id.id == item.uom_id.id)
+            if not ticket_line_ids:
+                continue
+            service_line_ticket_vals = {}
+            if 'uom_id' in vals:
+                service_line_ticket_vals.update({
+                    'uom_id': vals.get('uom_id')
+                })
+            if 'price' in vals:
+                service_line_ticket_vals.update({
+                    'price': vals.get('price')
+                })
+            if 'qty' in vals:
+                service_line_ticket_vals.update({
+                    'qty': vals.get('qty')
+                })
+            if 'amount_total' in vals:
+                service_line_ticket_vals.update({
+                    'amount_total': vals.get('amount_total')
+                })
+            ticket_line_ids.write(service_line_ticket_vals)
+        return super().write(vals)
