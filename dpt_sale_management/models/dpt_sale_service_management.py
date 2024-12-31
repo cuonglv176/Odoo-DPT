a432cce5c894e42f7865a5cb22f139a6aa3c7d1d
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2024-12-31 16:22:29 +0700

    Update dpt_sale_service_management.py

--- dpt_sale_management/models/dpt_sale_service_management.py
+++ dpt_sale_management/models/dpt_sale_service_management.py
@@ -26,20 +26,16 @@ class DPTSaleServiceManagement(models.Model):
     note = fields.Text(string='Note')
 
     def unlink(self):
-        records_to_delete = self.read(['service_id', 'qty', 'description'])
         res = super(DPTSaleServiceManagement, self).unlink()
-        for record in records_to_delete:
-            sale_id = record['sale_id'][0] if record['sale_id'] else None
-            if sale_id:
-                parent = self.env['sale.order'].browse(sale_id)
-                message = f"Dịch vụ bị xoá: {record['service_id']}"
-                parent.message_post(body=message, message_type='notification')
+        for record in self:
+            if record.sale_id:
+                message = f"Dịch vụ bị xoá: {record.service_id}"
+                record.sale_id.message_post(body=message, message_type='comment')
         return res
 
     def write(self, vals):
         old_values = {rec.id: rec.read(vals.keys())[0] for rec in self}
         res = super(DPTSaleServiceManagement, self).write(vals)
-
         self.action_confirm_quote()
         self.action_check_status_sale_order()
         for rec in self:
@@ -51,7 +47,7 @@ class DPTSaleServiceManagement(models.Model):
                         changes.append(f"{field}: {old_value} -> {new_value}")
                 if changes:
                     message = f"Thông tin dịch vụ thay đổi: {rec.service_id.name}: " + ", ".join(changes)
-                    rec.sale_id.message_post(body=message, message_type='notification')
+                    rec.sale_id.message_post(body=message, message_type='comment')
         return res
 
     def action_check_status_sale_order(self):
