0a058f410dac909b865f990d67cc5b0f3f1cd622
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2025-02-16 22:57:40 +0700

    update code

--- dpt_purchase_stock/wizards/stock_picking_return.py
+++ dpt_purchase_stock/wizards/stock_picking_return.py
@@ -14,22 +14,26 @@ class ReturnPicking(models.TransientModel):
                 [('product_id', '=', package_id.uom_id.product_id.id), ('name', '=', self.picking_id.picking_lot_name)])
             if not lot_id:
                 continue
-            return_line_id = self.product_return_moves.filtered(lambda rl: rl.product_id.id == package_id.uom_id.product_id.id)
+            return_line_id = self.product_return_moves.filtered(
+                lambda rl: rl.product_id.id == package_id.uom_id.product_id.id and rl.move_id == package_id.move_ids[
+                    0].id)
             if not return_line_id:
                 continue
-            package_vals.append((0, 0, {
-                'quantity': return_line_id.quantity,
-                'uom_id': package_id.uom_id.id,
-                'length': package_id.length,
-                'width': package_id.width,
-                'height': package_id.height,
-                'weight': package_id.weight,
-                'volume': package_id.volume,
-                'total_volume': math.ceil(round(package_id.volume * package_id.quantity * 100, 4)) / 100,
-                'total_weight': math.ceil(round(package_id.weight * package_id.quantity, 2)),
-                'sale_id': self.picking_id.sale_purchase_id.id,
-                'lot_id': lot_id.id,
-            }))
+            else:
+                return_line_id = return_line_id[0]
+                package_vals.append((0, 0, {
+                    'quantity': return_line_id.quantity,
+                    'uom_id': package_id.uom_id.id,
+                    'length': package_id.length,
+                    'width': package_id.width,
+                    'height': package_id.height,
+                    'weight': package_id.weight,
+                    'volume': package_id.volume,
+                    'total_volume': math.ceil(round(package_id.volume * package_id.quantity * 100, 4)) / 100,
+                    'total_weight': math.ceil(round(package_id.weight * package_id.quantity, 2)),
+                    'sale_id': self.picking_id.sale_purchase_id.id,
+                    'lot_id': lot_id.id,
+                }))
         if package_vals:
             res['package_ids'] = package_vals
         return res
