2ba40af7d4fb023e044c3e302d116e5a9c5b455b
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2025-03-11 00:10:32 +0700

    Update sale_order.py

--- dpt_sale_management/models/sale_order.py
+++ dpt_sale_management/models/sale_order.py
@@ -157,7 +157,7 @@ class SaleOrder(models.Model):
             #     continue
             for required_fields_id in sale_service_id.service_id.required_fields_ids:
                 if required_fields_id.id in list_exist:
-                    for field_data in self.env['sale.order'].browse(self.id.origin).fields_ids:
+                    for field_data in self.env['sale.order'].browse(self.origin).fields_ids:
                         if field_data.fields_id.id == required_fields_id.id:
                             val.append({
                                 'sequence': 1 if field_data.type == 'required' else 0,
