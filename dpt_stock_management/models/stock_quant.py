fabc28f6f2549e3eae797232a0e9c1e5592c04e0
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2025-03-15 09:26:42 +0700

    update code

--- dpt_stock_management/models/stock_quant.py
+++ dpt_stock_management/models/stock_quant.py
@@ -10,9 +10,10 @@ class StockQuant(models.Model):
     volume = fields.Float('Volume (m3)', related="lot_id.volume")
     total_weight = fields.Float('Total Weight (kg)', related="lot_id.total_weight")
     total_volume = fields.Float('Total Volume (m3)', related="lot_id.total_volume")
-    sale_id = fields.Many2one('sale.order', 'Sales', compute="_compute_sale_information")
+    sale_id = fields.Many2one('sale.order', 'Đơn hàng', compute="_compute_sale_information")
     employee_cs = fields.Many2one('hr.employee', string='Employee CS', compute="_compute_sale_information")
     partner_id = fields.Many2one('res.partner', string='Khách hàng', compute="_compute_sale_information")
+    packing_lot_name = fields.Char('Mã Pack', compute="_compute_sale_information")
 
     def _compute_sale_information(self):
         for item in self:
@@ -22,3 +23,4 @@ class StockQuant(models.Model):
             item.sale_id = sale_id
             item.employee_cs = sale_id.employee_cs if sale_id else None
             item.partner_id = sale_id.partner_id if sale_id else None
+            item.packing_lot_name = incoming_picking_id.packing_lot_name if incoming_picking_id else None
