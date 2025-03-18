5b8465a875eed61934d524093711954f404cd779
cuonglv176 <brianlee.lc@gmail.com>
2025-03-18 12:40:25 +0700

    Thời hạn thanh toán

--- dpt_account_payment_request/models/approval_request.py
+++ dpt_account_payment_request/models/approval_request.py
@@ -9,4 +9,4 @@ class ApprovalRequest(models.Model):
     _inherit = 'approval.request'
 
     payment_id = fields.Many2one('account.payment', string='ĐNTT')
-    is_payment_in_day = fields.Boolean(string='Thanh toán trong ngày',related='payment_id.is_payment_in_day')
+    payment_due = fields.Datetime(string='Thời hạn thanh toán',related='payment_id.payment_due')
