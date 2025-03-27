d2dda57079206991b21f600b5038452ed79767c8
cuonglv176 <brianlee.lc@gmail.com>
2025-03-27 11:30:14 +0700

    update

--- dpt_account_payment_request/models/approval_request.py
+++ dpt_account_payment_request/models/approval_request.py
@@ -9,4 +9,4 @@ class ApprovalRequest(models.Model):
     _inherit = 'approval.request'
 
     payment_id = fields.Many2one('account.payment', string='ĐNTT')
-    payment_due = fields.Datetime(string='Thời hạn thanh toán',related='payment_id.payment_due')
+    payment_due = fields.Datetime(string='Thời hạn thanh toán', related='payment_id.payment_due', store=True)
