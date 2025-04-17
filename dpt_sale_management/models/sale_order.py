88b09004e0bc0773f498f1f16f979c956f8600a2
Ưng Hoàng Tú <tuuh@jeisys.com.vn>
2025-04-17 11:36:19 +0700

    Update code

--- dpt_sale_management/models/sale_order.py
+++ dpt_sale_management/models/sale_order.py
@@ -461,10 +461,10 @@ class SaleOrder(models.Model):
                         max_price = price
                         price_list_item_id = service_price_id
                 elif service_price_id.compute_price == 'table':
-                    # compute_field_ids = self.fields_ids.filtered(
-                    #     lambda f: f.using_calculation_price and f.service_id.id == sale_service_id.service_id.id)
                     compute_field_ids = self.fields_ids.filtered(
-                        lambda f: f.using_calculation_price and f.sale_service_id.id == sale_service_id.id)
+                        lambda f: f.using_calculation_price and f.service_id.id == sale_service_id.service_id.id)
+                    # compute_field_ids = self.fields_ids.filtered(
+                    #     lambda f: f.using_calculation_price and f.sale_service_id.id == sale_service_id.id)
                     _logger.info("compute_field_ids %s", compute_field_ids)
                     for compute_field_id in compute_field_ids:
                         if not service_price_id.is_accumulated:
