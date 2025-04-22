11de1241f95746e8c920f30e3e24101381175f79
Ưng Hoàng Tú <tuuh@jeisys.com.vn>
2025-04-22 09:35:02 +0700

    Update code for Dunghq

--- dpt_export_import/__manifest__.py
+++ dpt_export_import/__manifest__.py
@@ -8,7 +8,7 @@
     'maintainer': 'Your Name',
     'website': 'http://dpt.com',
     'depends': ['sale', 'sale_management', 'dpt_service_management', 'dpt_purchase_management', 'hr', 'purchase',
-                'stock', 'dpt_purchase_stock', 'dpt_stock_management','dpt_security'],
+                'stock', 'dpt_purchase_stock', 'dpt_stock_management','dpt_security','dpt_helpdesk_ticket'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
