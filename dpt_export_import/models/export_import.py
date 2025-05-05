c8e61a771e442a17d59d1d97af7f96083c467e7a
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2025-05-05 12:06:34 +0700

    Update export_import.py

--- dpt_export_import/models/export_import.py
+++ dpt_export_import/models/export_import.py
@@ -234,6 +234,9 @@ class DptExportImport(models.Model):
         for sale_id in sale_ids:
             for dpt_export_import_line_id in sale_id.dpt_export_import_line_ids:
                 dpt_export_import_line_id.export_import_id = None
+        if 'payment_exchange_rate' in vals:
+            for line_id in self.line_ids:
+                line_id.dpt_exchange_rate = self.payment_exchange_rate
         return res
 
     @api.onchange('sale_ids')
