73d074c1b1622635cf4cb2e6924fb270a1859dbc
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2025-05-18 14:54:15 +0700

    update view

--- init_web_tree_view/__manifest__.py
+++ init_web_tree_view/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Hierarchy Tree View',
-    'version': '16.0.1.0.5',
+    'version': '17.0',  # Cập nhật phiên bản cho Odoo 17
     'category': 'Extra Tools',
     'summary': 'Hierarchy Tree View',
     'author': 'Init Co. Ltd',
     'support': 'contact@init.vn',
-    'website': 'https://init.vn/?utm_source=odoo-store&utm_medium=15&utm_campaign=hierarchy-tree-view',
+    'website': 'https://init.vn/?utm_source=odoo-store&utm_medium=17&utm_campaign=hierarchy-tree-view',  # Cập nhật URL
     'license': 'LGPL-3',
     'price': '29',
     'currency': 'USD',
@@ -29,9 +29,6 @@
 
         # security
     ],
-    'qweb': [],
-    'demo': [],
-    'test': [],
     'assets': {
         'web.assets_backend': [
             'init_web_tree_view/static/src/scss/tree_view.scss',
@@ -39,10 +36,13 @@
             'init_web_tree_view/static/src/js/tree_model.js',
             'init_web_tree_view/static/src/js/tree_renderer.js',
             'init_web_tree_view/static/src/js/tree_view.js',
-            'init_web_tree_view/static/src/xml/web_tree_view.xml',
-
         ],
-
+        'web.assets_web_dark': [
+            'init_web_tree_view/static/src/scss/tree_view_dark.scss',
+        ],
+        'web.assets_qweb': [
+            'init_web_tree_view/static/src/xml/web_tree_view.xml',
+        ],
     },
     'images': ['static/description/banner.png'],
     'installable': True,
