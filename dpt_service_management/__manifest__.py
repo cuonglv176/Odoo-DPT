a2a55eb4a7cd6fba39487307c7f1664780f5d6c0
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2025-05-18 23:49:06 +0700

    Update __manifest__.py

--- dpt_service_management/__manifest__.py
+++ dpt_service_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'DPT Service Management',
-    'version': '17.0.1',
+    'version': '17.0.1.0.0',
     'summary': 'Manage Services Information',
     'category': 'Services',
     'author': 'CuongLV',
