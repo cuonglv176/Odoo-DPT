3c26e69c918fdfe29839c8dbe761384e71bd3ab4
cuonglv176 <45865747+cuonglv176@users.noreply.github.com>
2025-05-20 22:26:27 +0700

    update view combo

--- dpt_sale_management/models/sale_order_service_combo.py
+++ dpt_sale_management/models/sale_order_service_combo.py
@@ -7,13 +7,18 @@ class ServiceCombo(models.Model):
     _description = 'Combo dịch vụ'
 
     combo_id = fields.Many2one('dpt.service.combo', string='Combo')
-    code = fields.Char('Mã combo', required=True)
+    code = fields.Char('Mã combo', related='combo_id.code')
     description = fields.Text('Mô tả')
     service_ids = fields.Many2many('dpt.service.management', string='Dịch vụ trong combo',
                                    related='combo_id.service_ids')
     sale_id = fields.Many2one('sale.order', string='Order')
     price = fields.Float('Giá combo', help='Để trống sẽ tính tổng từ các dịch vụ')
-    discount_percent = fields.Float('Giảm giá (%)', default=0.0)
+    discount_percent = fields.Float('Giảm giá', default=0.0)
     total_price = fields.Float('Tổng giá sau KM', compute='_compute_total_price')
     sale_service_ids = fields.One2many('dpt.sale.service.management', 'combo_id', 'Chi tiết dịch vụ')
 
+    @api.onchange('combo_id')
+    def onchange_combo_id(self):
+        if self.combo_id:
+            self.price = self.combo_id.price
+            self.discount_percent = self.combo_id.discount_percent
