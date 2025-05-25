efd2435f7193281c669cd693281060942763d2a0
Ưng Hoàng Tú <unghoangtu96@gmail.com>
2025-05-25 23:28:32 +0700

    update code

--- dpt_expense/models/purchase_order.py
+++ dpt_expense/models/purchase_order.py
@@ -12,13 +12,10 @@ class PurchaseOrder(models.Model):
     shipping_slip_ids = fields.Many2many('dpt.shipping.slip', string="Phiếu vận chuyển")
     order_expense_ids = fields.Many2many('sale.order', 'purchase_order_expense_rel', 'purchase_id', 'sale_id',
                                          string="Đơn hàng chi phí")
-    total_order_expense_ids = fields.Many2many('sale.order', string="Tât cả đơn hàng tính chi phí", store=True,
-                                               compute="_compute_order_expense", inverse="_inverse_order_expense")
+    total_order_expense_ids = fields.Many2many('sale.order', string="Tât cả đơn hàng tính chi phí", store=False,
+                                               compute="_compute_order_expense")
 
-    @api.depends('shipping_slip_ids')
+    @api.depends('shipping_slip_ids', 'order_expense_ids')
     def _compute_order_expense(self):
         for item in self:
-            item.total_order_expense_ids = item.shipping_slip_ids.mapped('sale_ids')
-
-    def _inverse_order_expense(self):
-        pass
+            item.total_order_expense_ids = item.shipping_slip_ids.mapped('sale_ids') | item.order_expense_ids
