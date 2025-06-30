17fa5f553e889442b5b15e0eeecac2a97c18764b
phuonganh1101 <dpt.intern07@gmail.com>
2025-06-30 11:18:01 +0700

    Xoá bỏ phân quyền dpt_project_task

--- dpt_project_task/__manifest__.py
+++ dpt_project_task/__manifest__.py
@@ -10,8 +10,6 @@
     'author': 'DPT',
     'depends': ['project'],
     'data': [
-        'security/project_security.xml',
-        'security/ir.model.access.csv',
         'views/project_task_views.xml',
         'views/project_task_menu.xml',
         'data/project_data.xml',
